67584397a97750fd37ba1ccbef30a3494fe2dee7
Dustin Collins <dustinrcollins@gmail.com>
2016-09-14 11:46:36 -0500

    Run tests in Docker

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
-gem install bundler
-bundle update
-env CONJUR_ENV=ci bundle exec rake spec
+rm -f Gemfile.lock
+
+docker run --rm \
+  -v $PWD:/usr/src/app \
+  -w /usr/src/app \
+  -e CONJUR_ENV=ci \
+  ruby:2.2.4 \
+  sh -c "bundle update && bundle exec rake spec"
